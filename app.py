349baf33ce1310444b7183d722a7fa486c6819a9
Oscar Hudson <hudso009@gmail.com>
2023-04-13 19:55:20 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,98 @@
+from flask import Flask, render_template, send_file
+import json, requests
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+
+def get_title_info(tid, type):
+
+    match type:
+        case "name":
+            #return "Super Mario"
+        
+            url = f"https://ninja.wup.shop.nintendo.net/ninja/ws/titles/id_pair?title_id[]={tid}"
+            data = requests.get(url, verify=False, cert="./static/WIIU_WOOD_1_CERT.pem").text
+
+            bs = BeautifulSoup(data, 'lxml')
+            ns_uid = bs.find("ns_uid")
+            
+            if ns_uid is not None:
+                url = f"https://samurai.wup.shop.nintendo.net/samurai/ws/GB/title/{ns_uid.text}/?shop_id=2"
+                data = requests.get(url, verify=False, cert="./static/WIIU_WOOD_1_CERT.pem").text
+
+                bs = BeautifulSoup(data, 'lxml')
+                print(url)
+
+                name = bs.find("name")
+
+                if name is not None:
+                    return name.text
+                else:
+                    return "Unknown"
+            else:
+                return "Unknown"
+        case "icon":
+            return f"https://caramelkat.github.io/Nintendo-eShop-Title-DB/titles/{tid}/icon.jpg"
+        case "code":
+            #return "Super Mario"
+        
+            url = f"https://ninja.wup.shop.nintendo.net/ninja/ws/titles/id_pair?title_id[]={tid}"
+            data = requests.get(url, verify=False, cert="./static/WIIU_WOOD_1_CERT.pem").text
+
+            bs = BeautifulSoup(data, 'lxml')
+            ns_uid = bs.find("ns_uid")
+
+            if ns_uid is not None:
+                url = f"https://samurai.wup.shop.nintendo.net/samurai/ws/GB/title/{ns_uid.text}/?shop_id=2"
+                data = requests.get(url, verify=False, cert="./static/WIIU_WOOD_1_CERT.pem").text
+
+                bs = BeautifulSoup(data, 'lxml')
+
+                product_code = bs.find("product_code")
+
+                if product_code is not None:
+                    return product_code.text
+                else:
+                    return "Unknown"
+            else:
+                return "Unknown"
+        case "rating":
+            return f"https://caramelkat.github.io/Nintendo-eShop-Title-DB/titles/{tid}/rating.jpg"
+
+    # https://caramelkat.github.io/Nintendo-eShop-Title-DB/
+
+def convert_minutes(minutes):
+    return f"{minutes // 60} hours, {minutes % 60} minutes"
+
+def get_date(days):
+    wiiu_day = datetime(2000, 1, 1)
+    correct_day = wiiu_day + timedelta(days=days)
+    formatted = correct_day.strftime("%A, %d %B %Y")
+
+    return formatted
+
+@app.route('/')
+def main():
+    with open('PlayStats.json', 'r') as f:
+        PlayStats = json.load(f)
+
+    entry_count = PlayStats['entry_count']
+    entries = PlayStats['entries']
+
+    return render_template('stats.html', get_title_name=get_title_info, convert_minutes=convert_minutes, getdate=get_date, count=entry_count, statslist=entries)
+
+@app.route('/<cssfile>.css')
+def css(cssfile):
+    return send_file(f'./static/style/{cssfile}.css')
+
+@app.route('/fonts/<font>')
+def font(font):
+    return send_file(f'./static/fonts/{font}')
+
+@app.route('/icons/<icon>')
+def icons(icon):
+    return send_file(f'./static/img/{icon}')
+
+if __name__ == '__main__':
+    app.run('192.168.0.69', 80, debug=True)
---
+++ log.py
@@ -0,0 +1,87 @@
+# https://gist.github.com/GaryOderNichts/3f137c031c3617e513a67fff5780b76e
+# https://gist.github.com/NWPlayer123/7e6233aee364796c55d85f143bba4bd1
+
+# Import required libraries
+import os, json
+import ftputil
+from struct import *
+
+# Declare FTP variables
+address = input("Enter the Wii U's IP address. Do not include the port.\n -> ")
+
+# Declare other variables
+userid = input("Enter the user ID of whom you would like to extract the save. Example: '80000003'.\n -> ")
+titleurl = "/storage_mlc/usr/save/system/pdm/[userid]/"
+
+logurl = titleurl.replace("[userid]", userid)
+
+def file_check():
+
+    if "saves" not in os.listdir():
+        os.mkdir("saves")
+
+    if "PlayStats.json" not in os.listdir():
+        with open("PlayStats.json", 'w') as f:
+            json.dump({"entry_count": 0, "entries": []}, f, indent=4)
+
+def transfer_saves():
+
+    with ftputil.FTPHost(address, "anonymous", "anonymous") as ftp_host:
+        print("\nConnected to Wii U!")
+
+        ftp_host.chdir(logurl)
+        names = ftp_host.listdir(ftp_host.curdir)
+
+        print(f"\nThere are {len(names) - 1} log files. They may take a while to download, so please be patient.\n")
+
+        for savefile in names:
+            print(f" -> Downloading '{savefile}'...")
+            ftp_host.download(f"{logurl}/{savefile}", f"./saves/{savefile}")
+
+        print("\nAll log files have been downloaded.")
+    
+    print("\nDisconnected from Wii U!")
+
+def PlayStats():
+
+    with open("saves/PlayStats.dat", 'rb') as f:
+        data = f.read()
+
+    entry_count = unpack('>I', data[0:4])[0]
+
+    with open('PlayStats.json', 'r') as f:
+        playstatsJson = json.load(f)
+
+    playstatsJson['entry_count'] = entry_count
+
+    for val in range(4, 20*entry_count, 20):
+        entry = unpack(">QIHHHH", data[val:val+20])
+
+        tid_hi = entry[0] >> 32
+        tid_lo = entry[0] & 0xFFFFFFFF
+        formatted_tid = f'{tid_hi:08x}{tid_lo:08x}'.upper()
+
+        entry_totaltime = entry[1]
+        entry_times = entry[2]
+
+        entry_firstplay = entry[3]
+        entry_lastplay = entry[4]
+
+        entry_dict = {
+            "tid": formatted_tid,
+            "time": entry_totaltime,
+            "count": entry_times,
+            "first": entry_firstplay,
+            "last": entry_lastplay
+        }
+
+        if entry_dict not in playstatsJson['entries']:
+            playstatsJson['entries'].append(entry_dict)
+
+    with open('PlayStats.json', 'w') as f:
+        json.dump(playstatsJson, f, indent=4)
+
+if __name__ == '__main__':
+    file_check()
+    transfer_saves()
+    PlayStats()
